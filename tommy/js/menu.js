e2f606123951bf37e6fe3f9e6cb50f4d4286c93a
PARKSANGHO-WEB <56953446+PARKSANGHO-WEB@users.noreply.github.com>
2019-11-24 23:30:25 +0900

    retommy

--- tommy/js/menu.js
+++ tommy/js/menu.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     let myNav = document.querySelector("#myNav");
     let container = document.querySelector(".containerN");
+    let html= document.querySelector("html");
     
     container.addEventListener("click", function() {
         myNav.classList.toggle("change");
+        html.classList.toggle("overflowy");
     })
     
 })
