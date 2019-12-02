7757b52d1b3114f4ea0f480d5a5c06d571d7f373
PARKSANGHO-WEB <56953446+PARKSANGHO-WEB@users.noreply.github.com>
2019-12-02 10:19:45 +0900

    sujung

--- js/main.js
+++ js/main.js
@@ -19,3 +19,16 @@ $(document).ready(function() {
 	}, 3000);
 	
 });
+
+
+ $(document).ready(function(){
+	   $(window).bind('scroll', function() {
+	   var navHeight = $( window ).height() - 70;
+			 if ($(window).scrollTop() > navHeight) {
+				 $('nav').addClass('fixed');
+			 }
+			 else {
+				 $('nav').removeClass('fixed');
+			 }
+		});
+});
